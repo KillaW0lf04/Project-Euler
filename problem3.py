1c449fd7f9874779e69d0f7a9c711b9a3d474523
Michael Aquilina <michaelaquilina@gmail.com>
2013-08-30 10:30:56 +0200

    Change problem3.py to use factorise from utils.py

--- problem3.py
+++ problem3.py
@@ -2,35 +2,12 @@
 #
 # What is the largest prime factor of the number 600851475143 ?
 
-
-def is_prime(n):
-
-    for i in xrange(2, n - 1):
-        if n % i == 0:
-            return False
-
-    return True
-
+from utils import factorise
 
 if __name__ == '__main__':
     value = 600851475143
-    sub_value = value
 
-    while True:
+    factors = factorise(value)
 
-        if is_prime(sub_value):
-            print sub_value, '... True'   # We have our result
-            break
-        else:
-            print sub_value, '.... False'
-
-        # Find the next number to divide by
-        for i in xrange(2, sub_value - 1):
-            if sub_value % i == 0:
-
-                print 'Dividing by %s' % i
-                sub_value /= i
-                break
-
-    print 'Finished'
+    print max(factors)
 
