057405e1bb41ae9389f99a83c45e7aab363571d3
Michael Aquilina <michaelaquilina@gmail.com>
2013-09-01 20:16:04 +0200

    Add solution to problem 9.

---
+++ problem9.py
@@ -0,0 +1,29 @@
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+#
+# a^2 + b^2 = c^2
+# For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+#
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+import math
+import sys
+
+
+if __name__ == '__main__':
+
+    a = 0
+    b = 0
+    c = 0
+    while True:
+        a = 0
+        b += 1
+
+        while a < b:
+            a += 1
+
+            c = math.sqrt(a ** 2 + b ** 2)
+
+            if a + b + c == 1000:
+                print '{}, {}, {}'.format(a, b, c)
+                print 'Result = {}'.format(a * b * c)
+                sys.exit(0)
